f5b1c6cfc4ae60b14fbe7fdacc20492668125a23
ralph <sanghwaj222@gmail.com>
2025-01-23 01:29:29 +0900

    [feat/#1] chore

---
+++ src/main/java/com/example/hipple/HippleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hipple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HippleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hipple/HippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hipple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
